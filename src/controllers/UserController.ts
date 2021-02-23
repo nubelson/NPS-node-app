96a250248232508db3152097f303f5aea6a5572a
Nubelson Fernandes <nubelsondev@hotmail.com>
2021-02-23 14:39:45 +0000

    feat(UserController): add create method

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,32 @@
 import { Request, Response } from 'express'
+import { getRepository } from 'typeorm'
+import { User } from '../models/User'
 
 class UserController {
 	async create(request: Request, response: Response) {
-		const { body } = request
+		const { name, email } = request.body
 
-		console.log(body)
-		response.send(body)
+		//* Criamos um repository (gerenciador de entidade) para o Model de usuários
+		const usersRepository = getRepository(User)
+
+		const userExists = await usersRepository.findOne({
+			email,
+		})
+
+		if (userExists) {
+			return response.status(400).json({
+				error: 'User already exists!',
+			})
+		}
+
+		const user = usersRepository.create({
+			name,
+			email,
+		})
+
+		await usersRepository.save(user)
+
+		return response.json(user)
 	}
 }
 
